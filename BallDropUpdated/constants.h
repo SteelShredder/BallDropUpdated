375938ede08b394bd1b57032c33639bdb2856dbb
Gavin Gavington <GavinGavington@gavins-macbook-air.local>
2018-09-30 11:26:53 -0700

    Start with template code

---
+++ BallDropUpdated/constants.h
@@ -0,0 +1,15 @@
+//
+//  constants.h
+//  BallDropUpdated
+//
+//  Created by Gavin Gavington on 9/30/18.
+//
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace myConstants
+{
+    const double gravity(9.8); // in meters/second squared
+}
+#endif
--- BallDropUpdated/main.cpp
+++ BallDropUpdated/main.cpp
@@ -6,9 +6,55 @@
 //
 
 #include <iostream>
+#include "constants.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// gets initial height from user and returns it
+double getInitialHeight()
+{
+    std::cout << "Enter the height of the tower in meters: ";
+    double initialHeight;
+    std::cin >> initialHeight;
+    return initialHeight;
+}
+
+// Returns height from ground after "secondsPassed" seconds
+double calculateHeight(double initialHeight, int secondsPassed)
+{
+    // Using formula: [ s = u * t + (a * t^2) / 2 ], here u(initial velocity) = 0
+    double distanceFallen = (myConstants::gravity * secondsPassed * secondsPassed) / 2;
+    double currentHeight = initialHeight - distanceFallen;
+    
+    return currentHeight;
+}
+
+// Prints height every second till ball has reached the ground
+void printHeight(double height, int secondsPassed)
+{
+    if (height > 0.0)
+    {
+        std::cout << "At " << secondsPassed << " seconds, the ball is at height:\t" << height <<
+        " meters\n";
+    }
+    else
+        std::cout << "At " << secondsPassed << " seconds, the ball is on the ground.\n";
+}
+
+void calculateAndPrintHeight(double initialHeight, int secondsPassed)
+{
+    double height = calculateHeight(initialHeight, secondsPassed);
+    printHeight(height, secondsPassed);
+}
+
+int main()
+{
+    const double initialHeight = getInitialHeight();
+    
+    calculateAndPrintHeight(initialHeight, 0);
+    calculateAndPrintHeight(initialHeight, 1);
+    calculateAndPrintHeight(initialHeight, 2);
+    calculateAndPrintHeight(initialHeight, 3);
+    calculateAndPrintHeight(initialHeight, 4);
+    calculateAndPrintHeight(initialHeight, 5);
+    
     return 0;
 }
